e84e2d2fb2d6e84e49a4f0be440c2e093e5171df
Vaibhav <Vaibhavhaswani@gmail.com>
2020-02-05 17:53:05 +0530

    Initial stable version of Snakes and Ladders

---
+++ Snakes/SnakeC/Cpr.java
@@ -0,0 +1,11 @@
+package Snakes.SnakeC;
+
+public class Cpr{
+	public Cpr(){
+		int[] asc={67, 111, 112, 121, 114, 105, 103, 104, 116, 32, 64, 32, 86, 97, 105, 98, 104, 97, 118, 32 ,72, 97, 115, 119, 97, 110, 105 };
+		System.out.print("\n\n\t\t\t\t\t\t\t");
+		for(int i:asc)
+			System.out.print((char)i);
+		System.out.println("\n\n");
+	}
+}
